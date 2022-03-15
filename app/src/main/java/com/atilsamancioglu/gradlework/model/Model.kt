0fd88c7d451a8d4470f7e032a25f05551dc20aa4
= <=enest5529@gmail.com>
2022-03-15 18:20:57 +0300

    init

---
+++ app/src/main/java/com/atilsamancioglu/gradlework/model/Model.kt
@@ -0,0 +1,13 @@
+package com.atilsamancioglu.gradlework.model
+
+data class Country
+    (
+
+    val countryName:String?,
+    val countryRegion:String?,
+    val countryLanguage:String?,
+    val countryCapital:String?,
+    val countryCurrency:String?,
+    val imageURL:String?
+
+    )
